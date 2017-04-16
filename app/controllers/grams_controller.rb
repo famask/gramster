63cfd5fa4cd92921cff278f6b2394ceebb66baee
Faisal <faisal.abdullah@gmail.com>
2017-04-16 08:00:23 +0000

    show action tests pass successfully

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,11 +1,18 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  
+
   def index
   end
 
   def new
     @gram = Gram.new
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
   end
 
   def create
@@ -16,6 +23,9 @@ class GramsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  
+
 end
 
 private
