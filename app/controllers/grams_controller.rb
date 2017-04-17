8024369db60c3a9322ace1a35780b56027ac708b
Faisal <faisal.abdullah@gmail.com>
2017-04-17 14:32:16 +0000

    CarrierWave gem installed for uploading images

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -24,6 +24,7 @@ class GramsController < ApplicationController
   end
 
   def index
+    @grams = Gram.all
   end
 
   def new
@@ -57,7 +58,7 @@ end
 private
 
 def gram_params
-  params.require(:gram).permit(:message)
+  params.require(:gram).permit(:message, :picture)
 end
 
 def render_not_found(status=:not_found)
